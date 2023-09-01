474981d45247dc5d20575978d6398ed707fd23a0
Ahmet <ahmetcaakirr@gmail.com>
2023-09-02 02:18:49 +0300

    Domain Files

---
+++ src/forwardingApi/Domain/Entities/Port.cs
@@ -0,0 +1,16 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+public class Port : Entity<int>
+{
+    public string PortName { get; set; }
+    public string PortCode { get; set; }
+    public string CountryCode { get; set; }
+    public string CountryName { get; set; }
+    public string Region { get; set; }
+}
